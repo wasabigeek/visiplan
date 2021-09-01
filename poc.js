197300146894591e920d2a5ecc19fef338fa435d
wasabigeek <wasabigeek@users.noreply.github.com>
2021-09-01 23:01:00 +0800

    Change to more like a game loop

--- poc.js
+++ poc.js
@@ -11,67 +11,49 @@ class Entry {
 
 // Income, Age => allocations to CPF
 const calculateSimpleCpfContribution = (options) => {
-  const { monthlyIncome, birthDate, startDate, retirementAge } = options;
+  const { monthlyIncome, currentAge, currentYear, retirementAge = 65 } = options;
+  if (currentAge >= retirementAge) {
+    return [];
+  }
 
   // TODO: change according to age
   // https://www.cpf.gov.sg/Employers/EmployerGuides/employer-guides/paying-cpf-contributions/cpf-contribution-and-allocation-rates
+  // const age = currentYear - birthDate.getFullYear();
   const ORDINARY_RATE = 0.23;
   const SPECIAL_RATE = 0.06;
   const MEDISAVE_RATE = 0.08;
+
   // Ordinary Wage ceiling is 6000
   // https://www.cpf.gov.sg//Employers/EmployerGuides/employer-guides/hiring-employees/cpf-contributions-for-your-employees#Item592
   const baseYearlyIncome = Math.min(monthlyIncome, 6000) * 12;
 
-  const age = startDate.getFullYear() - birthDate.getFullYear();
-  const remainingYears = retirementAge - age;
-
   let cpfEntries = [];
-  for (let i = 0; i < remainingYears; i++) {
-    cpfEntries.push(new Entry({ year: startDate.getFullYear() + i, amount: baseYearlyIncome * ORDINARY_RATE, category: 'cpf_oa' }));
-    cpfEntries.push(new Entry({ year: startDate.getFullYear() + i, amount: baseYearlyIncome * SPECIAL_RATE, category: 'cpf_sa' }));
-    cpfEntries.push(new Entry({ year: startDate.getFullYear() + i, amount: baseYearlyIncome * MEDISAVE_RATE, category: 'cpf_ma' }));
-  }
+  cpfEntries.push(new Entry({ year: currentYear, amount: baseYearlyIncome * ORDINARY_RATE, category: 'cpf_oa' }));
+  cpfEntries.push(new Entry({ year: currentYear, amount: baseYearlyIncome * SPECIAL_RATE, category: 'cpf_sa' }));
+  cpfEntries.push(new Entry({ year: currentYear, amount: baseYearlyIncome * MEDISAVE_RATE, category: 'cpf_ma' }));
 
   return cpfEntries;
 }
 
 // Interest Calculations
-// TODO: can still obtain interest after retirement, etc.
+// TODO: interest rate varies depending on age
 const calculateSimpleCpfInterest = (entries) => {
-  let cpfInterestEntries = [];
-
-  // assumed sorted by year, one entry per year
-  entries
-    .filter(entry => entry.category === 'cpf_oa')
-    .reduce((accCpf, entry) => {
-      accCpf += entry.amount;
-      const interest = accCpf * 0.025;
-      cpfInterestEntries.push(new Entry({ year: entry.year, amount: interest, category: 'cpf_oa' }));
-
-      return accCpf + interest;
-    }, 0);
+  const cpfOa = new Entry({ year: 2021, amount: 100000, category: 'cpf_oa' });
+  const cpfSa = new Entry({ year: 2021, amount: 35000, category: 'cpf_sa' });
+  const cpfMa = new Entry({ year: 2021, amount: 35000, category: 'cpf_ma' });
 
   entries
-    .filter(entry => entry.category === 'cpf_sa')
-    .reduce((accCpf, entry) => {
-      accCpf += entry.amount;
-      const interest = accCpf * 0.04;
-      cpfInterestEntries.push(new Entry({ year: entry.year, amount: interest, category: 'cpf_sa' }));
+    .forEach(entry => {
+      if (entry.category === 'cpf_oa') {
+        cpfOa.amount += entry.amount * 0.025;
+      } else if (entry.category === 'cpf_sa') {
+        cpfSa.amount += entry.amount * 0.04;
+      } else if (entry.category === 'cpf_ma') {
+        cpfMa.amount += entry.amount * 0.04;
+      }
+    });
 
-      return accCpf + interest;
-    }, 0);
-
-  entries
-    .filter(entry => entry.category === 'cpf_ma')
-    .reduce((accCpf, entry) => {
-      accCpf += entry.amount;
-      const interest = accCpf * 0.04;
-      cpfInterestEntries.push(new Entry({ year: entry.year, amount: interest, category: 'cpf_ma' }));
-
-      return accCpf + interest;
-    }, 0);
-
-  return cpfInterestEntries;
+  return [cpfOa, cpfSa, cpfMa];
 }
 
 // TODO:
@@ -86,9 +68,24 @@ let entries = [
   new Entry({ year: 2021, amount: 35000, category: 'cpf_ma' })
 ]
 const CURRENT_INCOME = 10000;
-entries = entries.concat(calculateSimpleCpfContribution({ monthlyIncome: CURRENT_INCOME, birthDate: new Date('01/01/1987'), startDate: new Date('01/01/2021'), retirementAge: 65 }));
-// console.log(entries);
-entries = entries.concat(calculateSimpleCpfInterest(entries));
+const birthDate = new Date('01/01/1987');
+const lifeExpectancy = 95;
+let currentYear = (new Date()).getFullYear();
+let currentAge = currentYear - birthDate.getFullYear();
+let remainingYears = lifeExpectancy - currentAge;
+while (remainingYears > 0) {
+  // calculate regular transactions
+  entries = entries.concat(
+    calculateSimpleCpfContribution({ monthlyIncome: CURRENT_INCOME, currentAge, currentYear })
+  )
+  // calculate interest transactions
+  entries = entries.concat(calculateSimpleCpfInterest(entries));
+
+  currentAge++;
+  currentYear++;
+  remainingYears--;
+}
+
 
 // aggregate the data
 console.log(entries.reduce((acc, entry) => {
