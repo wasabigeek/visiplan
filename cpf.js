0db8df067c82325434a1a67eda3d7f7d930fb091
wasabigeek <wasabigeek@users.noreply.github.com>
2021-09-12 14:32:46 +0800

    Add OA calculations for other age ranges

--- cpf.js
+++ cpf.js
@@ -3,10 +3,20 @@ const roundMoney = (amount) => {
 }
 
 // https://www.cpf.gov.sg/Assets/employers/Documents/Table%2011_Pte%20and%20Npen_CPF%20Allocation%20Rates%20Jan%202016.pdf
-export const calculateCpfOaContribution = (age, income) => {
+export const calculateMonthlyCpfOaContribution = (age, income) => {
   let contribution = 0;
 
-  if (age > 35) {
+  if (age > 65) {
+    contribution = 0.08 * income;
+  } else if (age > 60) {
+    contribution = 0.2122 * income;
+  } else if (age > 55) {
+    contribution = 0.4616 * income;
+  } else if (age > 50) {
+    contribution = 0.4055 * income;
+  } else if (age > 45) {
+    contribution = 0.5136 * income;
+  } else if (age > 35) {
     contribution = 0.5677 * income;
   } else {
     contribution = 0.6217 * income;
@@ -26,7 +36,7 @@ export class CpfComponent {
 
   apply_yearly_updates() {
     const cpfOa = this.accountStore.get('cpf_oa');
-    cpfOa.add_entry(calculateCpfOaContribution(this.config.age, this.config.income));
+    cpfOa.add_entry(calculateMonthlyCpfOaContribution(this.config.age, this.config.income));
   }
 
   apply_yearly_interest() {
--- cpf.test.js
+++ cpf.test.js
@@ -1,11 +1,36 @@
-import { calculateCpfOaContribution } from "./cpf.js";
+import { calculateMonthlyCpfOaContribution } from "./cpf.js";
 
-test('calculateCpfOaContribution below 35 years old', () => {
-  const { amount } = calculateCpfOaContribution(25, 100);
+test('calculateMonthlyCpfOaContribution below 35 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(25, 100);
   expect(amount).toBe(62.17);
 })
 
-test('calculateCpfOaContribution at 36-45 years old', () => {
-  const { amount } = calculateCpfOaContribution(36, 100);
+test('calculateMonthlyCpfOaContribution at 36-45 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(36, 100);
   expect(amount).toBe(56.77);
+})
+
+test('calculateMonthlyCpfOaContribution at 46-50 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(46, 100);
+  expect(amount).toBe(51.36);
+})
+
+test('calculateMonthlyCpfOaContribution at 51-55 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(51, 100);
+  expect(amount).toBe(40.55);
+})
+
+test('calculateMonthlyCpfOaContribution at 56-60 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(56, 100);
+  expect(amount).toBe(46.16);
+})
+
+test('calculateMonthlyCpfOaContribution at 61-65 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(61, 100);
+  expect(amount).toBe(21.22);
+})
+
+test('calculateMonthlyCpfOaContribution at >65 years old', () => {
+  const { amount } = calculateMonthlyCpfOaContribution(66, 100);
+  expect(amount).toBe(8);
 })
