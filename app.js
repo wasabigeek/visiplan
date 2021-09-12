d0d6a899a0525286b117d4e52fa028ecd59516a1
wasabigeek <wasabigeek@users.noreply.github.com>
2021-09-12 14:43:44 +0800

    Fix: CPF calculation should be against contribution, not raw income

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import { AccountStore } from "./account.js";
 import { CpfComponent } from "./cpf.js";
 
 const accountStore = new AccountStore(); // pass in person?
+// TODO: should pass in DOB instead
 const cpfComponent = new CpfComponent(accountStore, { age: 35, income: 5000 })
 
 for (let year = 2021; year <= 2021; year++) {
   for (let month = 1; month <= 12; month++) {
+    // pass in year and month
     cpfComponent.apply_monthly_updates();
+    // pass in year and month
     // refactor to await: must wait for yearly updates to be applied first
     cpfComponent.apply_monthly_interest();
   }
--- cpf.js
+++ cpf.js
@@ -3,23 +3,23 @@ const roundMoney = (amount) => {
 }
 
 // https://www.cpf.gov.sg/Assets/employers/Documents/Table%2011_Pte%20and%20Npen_CPF%20Allocation%20Rates%20Jan%202016.pdf
-export const calculateMonthlyCpfOaContribution = (age, income) => {
+export const calculateMonthlyCpfOaContribution = (age, totalCpfContribution) => {
   let contribution = 0;
 
   if (age > 65) {
-    contribution = 0.08 * income;
+    contribution = 0.08 * totalCpfContribution;
   } else if (age > 60) {
-    contribution = 0.2122 * income;
+    contribution = 0.2122 * totalCpfContribution;
   } else if (age > 55) {
-    contribution = 0.4616 * income;
+    contribution = 0.4616 * totalCpfContribution;
   } else if (age > 50) {
-    contribution = 0.4055 * income;
+    contribution = 0.4055 * totalCpfContribution;
   } else if (age > 45) {
-    contribution = 0.5136 * income;
+    contribution = 0.5136 * totalCpfContribution;
   } else if (age > 35) {
-    contribution = 0.5677 * income;
+    contribution = 0.5677 * totalCpfContribution;
   } else {
-    contribution = 0.6217 * income;
+    contribution = 0.6217 * totalCpfContribution;
   }
   return { amount: roundMoney(contribution), dateTime: new Date() }
 }
@@ -36,7 +36,13 @@ export class CpfComponent {
 
   apply_monthly_updates() {
     const cpfOa = this.accountStore.get('cpf_oa');
-    cpfOa.add_entry(calculateMonthlyCpfOaContribution(this.config.age, this.config.income));
+    cpfOa.add_entry(
+      calculateMonthlyCpfOaContribution(
+        this.config.age,
+        // this is a naive calculation, should be: https://www.cpf.gov.sg/Assets/employers/Documents/Table%201_Pte%20and%20Npen%20CPF%20contribution%20rates%20for%20Singapore%20Citizens%20and%203rd%20year%20SPR%20Jan%202016.pdf
+        this.config.income * 0.37
+      )
+    );
   }
 
   apply_monthly_interest() {
