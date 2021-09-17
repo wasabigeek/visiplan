626e03b33e56770f28616f96b501a2e2f82a7282
wasabigeek <wasabigeek@users.noreply.github.com>
2021-09-17 22:25:48 +0800

    refactor: Nest the tests

--- simulators/cpf/tests/index.test.js
+++ simulators/cpf/tests/index.test.js
@@ -2,29 +2,30 @@ import { CpfSim } from "../index.js";
 import { AccountStore } from "../../../account.js"; // TODO: mock
 import Person from "../../../person.js";
 
-// Naive Integration Tests
-test('it calculates CPF OA correctly', () => {
-  const accountStore = new AccountStore();
-  const person = new Person(new Date(1987, 0, 1));
+describe('apply_monthly_updates()', () => {
+  test('it calculates CPF OA correctly', () => {
+    const accountStore = new AccountStore();
+    const person = new Person(new Date(1987, 0, 1));
 
-  const cpfSim = new CpfSim({ accountStore, person }, { income: 5000 });
+    const cpfSim = new CpfSim({ accountStore, person }, { income: 5000 });
 
-  cpfSim.apply_monthly_updates({ monthStart: new Date(2022, 7, 14) });
+    cpfSim.apply_monthly_updates({ monthStart: new Date(2022, 7, 14) });
 
-  const oaBalance = accountStore.get("cpf_oa").current_balance();
-  expect(oaBalance).toBe(1150.15);
-});
-test('it generates OA entries with the correct date', () => {
-  const accountStore = new AccountStore();
-  const person = new Person(new Date(1987, 0, 1));
+    const oaBalance = accountStore.get("cpf_oa").current_balance();
+    expect(oaBalance).toBe(1150.15);
+  });
+  test('it generates OA entries with the correct date', () => {
+    const accountStore = new AccountStore();
+    const person = new Person(new Date(1987, 0, 1));
 
-  const cpfSim = new CpfSim({ accountStore, person }, { income: 5000 });
+    const cpfSim = new CpfSim({ accountStore, person }, { income: 5000 });
 
-  cpfSim.apply_monthly_updates({ monthStart: new Date(2022, 7, 14) });
+    cpfSim.apply_monthly_updates({ monthStart: new Date(2022, 7, 14) });
 
-  const dateTime = accountStore
-    .get("cpf_oa")
-    .entries[0]
-    .dateTime;
-  expect(dateTime.toISOString()).toBe(new Date(2022, 7, 14).toISOString());
+    const dateTime = accountStore
+      .get("cpf_oa")
+      .entries[0]
+      .dateTime;
+    expect(dateTime.toISOString()).toBe(new Date(2022, 7, 14).toISOString());
+  });
 });
