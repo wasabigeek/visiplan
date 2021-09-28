261ed515db995ad02f37d94d3e747e2b528b151c
wasabigeek <wasabigeek@users.noreply.github.com>
2021-09-28 21:54:20 +0800

    Add test for downpayment

--- simulators/hdb/tests/HdbWithHdbLoanSim.test.js
+++ simulators/hdb/tests/HdbWithHdbLoanSim.test.js
@@ -12,7 +12,18 @@ describe('HdbWithHdbLoanSim apply_yearly_updates', () => {
     sim.apply_yearly_updates({ yearStart: new Date(2022, 0) });
 
     const cashEntries = accountStore.get("cash").entries;
-    console.log(cashEntries)
     expect(cashEntries.find(entry => entry.amount == -2000)).toBeDefined();
+  });
+  test('it adds the downpayment cost', () => {
+    const accountStore = new AccountStore();
+    const person = new Person(new Date(1987, 0, 1));
+
+    const sim = new HdbWithHdbLoanSim({ accountStore, person }, { downpaymentYear: 2022, purchasePrice: 100000 });
+
+    sim.apply_yearly_updates({ yearStart: new Date(2022, 0) });
+
+    const cashEntries = accountStore.get("cash").entries;
+    const matchingEntry = cashEntries.find(entry => entry.amount == -10000 && entry.dateTime.getFullYear() == 2022);
+    expect(matchingEntry).toBeDefined();
   });
 });
