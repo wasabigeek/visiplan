f62705036b12fc61e2fa59da058e0c0299b1b0e5
wasabigeek <wasabigeek@users.noreply.github.com>
2021-10-03 16:46:49 +0800

    Remove SimpleSalary Schedule

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ import AsciiChart from "asciichart";
 
 import { AccountStore } from "./entities/account.js";
 import Person from "./entities/person.js";
-import SimpleSalary from "./entities/SimpleSalary.js";
 import { CpfSalaryContributionSim } from "./simulators/cpf/CpfSalaryContributionSim.js";
 import { HdbWithHdbLoanSim } from "./simulators/hdb/HdbWithHdbLoanSim.js";
 import SimpleExpensesSim from "./simulators/SimpleExpensesSim.js";
 import { SimpleInvestmentSim } from "./simulators/SimpleInvestmentSim.js";
 import SimpleSalarySim from "./simulators/SimpleSalarySim.js";
 
-const salarySchedule = new SimpleSalary({ startingYear: 2022, endYear: 2055, startingSalary: 5000 })
-const person = new Person({ birthDate: new Date(2000, 5, 1), salarySchedule });
+const person = new Person({ birthDate: new Date(2000, 5, 1) });
 const accountStore = new AccountStore();
 const baseConfig = { accountStore, person, startDate: new Date(2021, 0) }
 
