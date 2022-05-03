7062e1c0ad50a5d532b84b504ddca6a58c5c1d95
wasabigeek <wasabigeek@users.noreply.github.com>
2022-05-03 15:14:40 +0800

    Remove unused code from app.js

--- app.js
+++ app.js
@@ -2,31 +2,11 @@ import AsciiChart from "asciichart";
 
 import { AccountStore } from "./entities/account.js";
 import Person from "./entities/person.js";
-import CpfSim from "./simulators/cpf/CpfSim.js";
-import { HdbWithHdbLoanSim } from "./simulators/hdb/HdbWithHdbLoanSim.js";
-import SimpleExpensesSim from "./simulators/SimpleExpensesSim.js";
-import SimpleInvestmentSim from "./simulators/SimpleInvestmentSim.js";
-import SimpleSalarySim from "./simulators/SimpleSalarySim.js";
+
 
 const LIFE_EXPECTANCY = 90;
 const START_YEAR = 2021;
 const BIRTH_DATE = new Date(1995, 5, 1);
-
-const person = new Person({ birthDate: BIRTH_DATE });
-const accountStore = new AccountStore();
-accountStore.get("cash").add_entry({ amount: 5000, dateTime: new Date(START_YEAR, 0), title: "initial_cash" })
-const baseConfig = { accountStore, person, startDate: new Date(START_YEAR, 0) }
-
-const salarySim = new SimpleSalarySim(baseConfig, { baseSalary: 5000, growthRate: 0.025 });
-const expensesSim = new SimpleExpensesSim(baseConfig, { baseExpense: 3000 });
-const cpfSalaryContributionSim = new CpfSim({ accountStore, person }, { income: 5000 })
-const simpleInvestmentSim = new SimpleInvestmentSim({ accountStore, person }, { monthlyDeposit: 1000, perAnnumInterestRate: 0.05, drawdownRate: 0.03 });
-const hdbSim = new HdbWithHdbLoanSim(
-  { accountStore, person },
-  { downpaymentYear: 2022, purchasePrice: 450000, perAnnumInterestRate: 0.025, loanYears: 15, estimatedTopYear: 2026 }
-);
-
-
 const simulationEndYear = LIFE_EXPECTANCY + BIRTH_DATE.getFullYear();
 
 const initSimulators = (sims, simConfigs, baseConfig) => {
